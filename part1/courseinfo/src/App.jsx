a4ee5a7fdc9a452f3e896b19ddd7ce0a8bf80a3c
Citron <contact@youceflcv.fr>
2024-11-04 12:48:44 +0100

    Part 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,28 +3,29 @@ import Content from "./Content.jsx";
 import Header from "./Header.jsx";
 
 const App = () => {
-    const course = 'Half Stack application development'
-
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total exercises={parts.map((part) => part.exercises)} />
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total exercises={course.parts.map((part) => part.exercises)} />
         </div>
     )
 }
