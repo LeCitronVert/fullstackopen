a65111d46dbfb5bf811b4e243354dd1e1d0a6912
Citron <contact@youceflcv.fr>
2024-11-04 22:42:24 +0100

    Part 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import Person from "../components/Person.jsx";
 import Search from "../components/Search.jsx";
 import Persons from "../components/Persons.jsx";
 import PersonForm from "../components/PersonForm.jsx";
+import axios from "axios";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newPhone, setNewPhone] = useState('')
 
@@ -45,6 +41,20 @@ const App = () => {
         ? persons
         : persons.filter((person) => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
     ;
+
+
+    const fetchEffect = () => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then((response) => {
+                setPersons(response.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    useEffect(fetchEffect, [])
 
 
     return (
